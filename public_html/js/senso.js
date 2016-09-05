5d5241f4d1228c2f65eb2956181204760968ce4e
sunny <sunny@sunny>
2016-09-05 08:27:21 +0200

    ausgangsversion 05.09.2016

--- public_html/js/senso.js
+++ public_html/js/senso.js
@@ -9,43 +9,75 @@ function Farbknopf(obj, leuchtfarbe) {
 }
 
 Farbknopf.deaktiviereClick = function () {
-    $('.farbknopf').unbind('mousedown');
-    $('.farbknopf').unbind('mouseup');
+//    $('.farbknopf').unbind('mousedown');
+//    $('.farbknopf').unbind('mouseup');
+    $('.farbknopf').unbind('click');
+    $('.farbknopf').css('cursor', 'default');
 };
 
 Farbknopf.aktiviereClick = function () {
-    $('#gelb').mousedown(function () {
-        $(this).css('background-color', '#ffff00');
+    $('.farbknopf').css('cursor', 'pointer');
+//    $('#gelb').mousedown(function () {
+//        $(this).css('background-color', '#ffff00');
+//    });
+//    $('#gelb').mouseup(function () {
+//        $(this).css('background-color', '#cccc00');
+//        pruefeUserreaktion();
+//    });
+    $('#gelb').click(function () {
+        Farbknopf.leuchten2(this);
     });
-    $('#gelb').mouseup(function () {
-        $(this).css('background-color', '#cccc00');
+//    $('#rot').mousedown(function () {
+//        $(this).css('background-color', '#ff3333');
+//    });
+//    $('#rot').mouseup(function () {
+//        $(this).css('background-color', '#cc0000');
+//        pruefeUserreaktion();
+//    });
+    $('#rot').click(function () {
+        Farbknopf.leuchten2(this);
     });
-    $('#rot').mousedown(function () {
-        $(this).css('background-color', '#ff3333');
+//    $('#blau').mousedown(function () {
+//        $(this).css('background-color', '#0066ff');
+//    });
+//    $('#blau').mouseup(function () {
+//        $(this).css('background-color', '#0000cc');
+//        pruefeUserreaktion();
+//    });
+    $('#blau').click(function () {
+        Farbknopf.leuchten2(this);
     });
-    $('#rot').mouseup(function () {
-        $(this).css('background-color', '#cc0000');
+//    $('#gruen').mousedown(function () {
+//        $(this).css('background-color', '#00ff00');
+//    });
+//    $('#gruen').mouseup(function () {
+//        $(this).css('background-color', '#00cc00');
+//        pruefeUserreaktion();
+//    });
+    $('#gruen').click(function () {
+        Farbknopf.leuchten2(this);
     });
-    $('#blau').mousedown(function () {
-        $(this).css('background-color', '#0066ff');
-    });
-    $('#blau').mouseup(function () {
-        $(this).css('background-color', '#0000cc');
-    });
-    $('#gruen').mousedown(function () {
-        $(this).css('background-color', '#00ff00');
-    });
-    $('#gruen').mouseup(function () {
-        $(this).css('background-color', '#00cc00');
+};
+
+Farbknopf.leuchten2 = function (obj) {
+    Farbknopf.deaktiviereClick();
+    var id = '#' + obj.id;
+    var farbknopf = 'knopf' + id.charAt(1).toUpperCase() + id.substring(2, id.length);
+    var standardfarbe = $(id).css('background-color');
+    $(id).animate({'background-color': window[farbknopf].leuchtfarbe}, 100);
+    $(id).animate({'background-color': window[farbknopf].leuchtfarbe}, 150);
+    $(id).animate({'background-color': standardfarbe}, 100);
+    $(id).animate({'background-color': standardfarbe}, 300, function (){
+        pruefeUserreaktion(window[farbknopf]);
     });
 };
 
 Farbknopf.prototype.leuchten = function (next) {
     var standardfarbe = this.obj.css('background-color');
+    this.obj.animate({'background-color': this.leuchtfarbe}, 100);
     this.obj.animate({'background-color': this.leuchtfarbe}, 150);
-    this.obj.animate({'background-color': this.leuchtfarbe}, 200);
-    this.obj.animate({'background-color': standardfarbe}, 150);
-    this.obj.animate({'background-color': standardfarbe}, 500, next);
+    this.obj.animate({'background-color': standardfarbe}, 100);
+    this.obj.animate({'background-color': standardfarbe}, 300, next);
 };
 
 // Groß weil KLasse
